89d042ce216a3102b5f524257c458e51597007a0
Michael Fitz-Payne <mfitz.payne@gmail.com>
2018-08-22 12:59:34 +1000

    Add tests for the config file parsing logic.

--- main_test.go
+++ main_test.go
@@ -55,3 +55,70 @@ func TestValidate(t *testing.T) {
 	}
 
 }
+
+func TestConfig(t *testing.T) {
+	testTable := []struct {
+		config []byte
+		err    error
+	}{
+		{
+			config: []byte{},
+			err:    errors.New(errConfig),
+		},
+		{
+			config: []byte("Invalid JSON"),
+			err:    errors.New(errConfig),
+		},
+		{
+			config: []byte("[{}]"),
+			err:    errors.New(errConfigInvalid),
+		},
+		{
+			config: []byte(`[
+				{
+					"filepath":"/var/log/messages",
+					"delimiter":" ",
+					"regexp":"MATCHTHIS.*",
+					"command":"redis-cli",
+					"args":"publish key 'value'"
+				}
+			]`),
+		},
+		{
+			config: []byte(`[
+				{
+					"filepath":"/var/log/messages",
+					"delimiter":" ",
+					"regexp":"MATCHTHIS.*",
+					"command":"redis-cli",
+					"args":"publish key 'value'"
+				},
+				{
+					"filepath":"/var/log/nginx.log",
+					"regexp":"GET.*\\ 200\\ ",
+					"command":"redis-cli",
+					"args":"publish key 'other value''"
+				}
+			]`),
+		},
+	}
+
+	for _, table := range testTable {
+		_, retErr := parseConfigFile(table.config)
+		if retErr == nil && table.err == nil {
+			continue
+		}
+		if retErr == nil && table.err != nil {
+			t.Errorf("No error returned, expected %v", table.err)
+			continue
+		}
+		if retErr != nil && table.err == nil {
+			t.Errorf("Error returned as %v, expected nil.", retErr)
+			continue
+		}
+		if retErr.Error() != table.err.Error() {
+			t.Errorf("Error type was incorrect, got %v, want %v.", retErr.Error(), table.err)
+		}
+	}
+
+}
